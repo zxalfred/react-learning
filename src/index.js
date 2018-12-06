ecb4222067a463f1ebfe4a27dbeda576835ba429
Alfred <alfred.zhao@dragonsoftbravo.com>
2018-12-06 22:50:58 +0800

    event

--- src/index.js
+++ src/index.js
@@ -78,8 +78,69 @@ function Greeting(props) {
   return <GuestGreeting />;
 }
 
+function LoginButton(props) {
+  return (
+    <button onDoubleClick={props.onEvent}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onDoubleClick={props.onEvent}>
+      Logout
+    </button>
+  );
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
+
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+
+    if (isLoggedIn) {
+      button = <LogoutButton onEvent={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onEvent={this.handleLoginClick} />;
+    }
+
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    )
+  }
+}
+
+function MailBox(props) {
+  const unreadMessages = props.unreadMessages;
+  return (
+    <div>
+      <h1>Hello!</h1>
+      {}
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <Greeting isLoggedIn={true} />,
+  <LoginControl />,
   document.getElementById('root')
 );
 
