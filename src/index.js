551f7e3744cd5ea04b8c49cfdd03b6dc0bda637f
Alfred <alfred.zhao@dragonsoftbravo.com>
2018-12-06 13:45:30 +0800

    条件判断

--- src/index.js
+++ src/index.js
@@ -37,8 +37,49 @@ class Clock extends React.Component {
   }
 }
 
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: true };
+
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState((state) => {
+      return {
+        isToggleOn: !state.isToggleOn
+      };
+    });
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick.bind(this)}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+
 ReactDOM.render(
-  <Clock />,
+  <Greeting isLoggedIn={true} />,
   document.getElementById('root')
 );
 
