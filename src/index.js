801ec38938f5732dd044d76580c07bba6af236fe
zxsprout@foxmail.com <alfred.zhao@dragonsoftbravo.com>
2018-12-30 11:35:41 +0800

    multiple select

--- src/index.js
+++ src/index.js
@@ -4,29 +4,52 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function FancyBorder(props) {
-  return (
-    <div className={'FancyBorder FancyBorder-' + props.color}>
-      {props.children}
-    </div>
-  )
-}
+class FlavorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: []};
 
-function WelcomeDialog() {
-  return (
-    <FancyBorder color="blue">
-      <h1 className="Dialog-title">
-        Welcome
-      </h1>
-      <p className="Dialog-message">
-        Thank you for visiting our spacecraft!
-      </p>
-    </FancyBorder>
-  );
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const options = event.target.options;
+    console.log(options);
+    const value = [];
+    for (let i = 0, l = options.length; i < l; i++) {
+      if (options[i].selected) {
+        value.push(options[i].value);
+      }
+    }
+    this.setState({value})
+  }
+
+  handleSubmit(event) {
+    alert(`Your favorite flavor is: ${this.state.value}`);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite flavor:
+          <select multiple={true} value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 ReactDOM.render(
-  <WelcomeDialog />,
+  <FlavorForm />,
   document.getElementById('root')
 );
 
